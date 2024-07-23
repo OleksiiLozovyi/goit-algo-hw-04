d4d0ede182ca295d3136e86106e1498e26119fcf
LDLPROJECT\oleksii.lozovyi <oleksii.lozovyi@pragmaticplay.com>
2024-07-23 14:31:51 +0300

    HW4 (1-4) upload

--- HW4_2.py
+++ HW4_2.py
@@ -3,8 +3,9 @@ def get_cats_info(path):
     try:
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
-                id, name, age = line.split(',')
+                id, name, age = line.replace("\n","").split(",")
                 cats_info.append({"id": id, "name": name, "age": age})
+
         return cats_info
 
     except FileNotFoundError:
